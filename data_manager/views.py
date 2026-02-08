8a2c4c3c83073926ea535f90f5d683c0ddd9d392
Argun Varan <argunvaran@gmail.com>
2026-02-08 06:10:35 +0300

    Fix template syntax error (spacing around ==) via direct content replacement

--- data_manager/views.py
+++ data_manager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
